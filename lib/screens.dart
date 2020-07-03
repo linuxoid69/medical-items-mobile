285df967591a3119a6fb1cd0c8c07adcf24c90c9
Rustam Tagaev <rustam.tagaev@veeam.com>
2020-07-03 17:58:39 +0300

    added scanner

--- lib/screens.dart
+++ lib/screens.dart
@@ -1,8 +1,18 @@
 library screens;
 
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  var allCodes = [];
+  var monVal = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,31 +22,57 @@ class MainScreen extends StatelessWidget {
         title: Text('База медикаментов'),
       ),
       body: Container(
+        color: Colors.green[100],
         child: Table(
           children: [
-            TableRow(
-              children: [
+            TableRow(children: [
               RaisedButton(
                 child: Text('Сканировать товар.'),
-                onPressed: () {
-                  // Navigate to the second screen when tapped.
-                  Navigator.pushNamed(context, '/scan_barcode');
-                },
+                onPressed: scan,
               ),
               RaisedButton(
                 child: Text('Список лекарств в базе.'),
                 onPressed: () {
-                  // Navigate to the second screen when tapped.
                   Navigator.pushNamed(context, '/drugs_list');
                 },
               ),
-            ])
+            ]),
+            for (var i in allCodes)
+              TableRow(children: [
+                Text(i),
+                Checkbox(
+                  key: Key(i.toString()),
+                  value: monVal,
+                  onChanged: (bool value) {
+                    setState(() {
+                      monVal = value;
+                    });
+                  },
+                )
+              ])
           ],
         ),
       ),
     );
   }
+
+  Future scan() async {
+    var options = ScanOptions(
+      android: AndroidOptions(
+        useAutoFocus: true,
+      ),
+    );
+
+    var result = await BarcodeScanner.scan(options: options);
+
+    if (result.type.toString() != "Cancelled") {
+      allCodes.add(result.rawContent);
+    }
+    setState(() {});
+    return allCodes;
+  }
 }
+
 
 class DrugListScreen extends StatelessWidget {
   @override
@@ -46,10 +82,9 @@ class DrugListScreen extends StatelessWidget {
         backgroundColor: Colors.green,
         title: Text("Список лекарств"),
       ),
-      body: Center(
+      body: Container(
         child: RaisedButton(
           onPressed: () {
-            // Navigate back to first screen when tapped.
             Navigator.pushNamed(context, '/');
           },
           child: Text('Go to thee!'),

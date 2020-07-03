480ca16cec3c519423cdcc3c5d60e6da98a73f43
Rustam Tagaev <rustam.tagaev@veeam.com>
2020-07-04 00:59:59 +0300

    add scroll and clean list of drugs

--- lib/screens.dart
+++ lib/screens.dart
@@ -1,8 +1,11 @@
 library screens;
 
 import 'dart:async';
-import 'package:flutter/material.dart';
+
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -15,45 +18,100 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        centerTitle: true,
-        title: Text('База медикаментов'),
-      ),
-      body: Container(
-        color: Colors.green[100],
-        child: Table(
-          children: [
-            TableRow(children: [
-              RaisedButton(
-                child: Text('Сканировать товар.'),
-                onPressed: scan,
-              ),
-              RaisedButton(
-                child: Text('Список лекарств в базе.'),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/drugs_list');
-                },
-              ),
-            ]),
-            for (var i in allCodes)
-              TableRow(children: [
-                Text(i),
-                Checkbox(
-                  key: Key(i.toString()),
-                  value: monVal,
-                  onChanged: (bool value) {
-                    setState(() {
-                      monVal = value;
-                    });
-                  },
-                )
-              ])
-          ],
+    return new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.green,
+          centerTitle: true,
+          title: new Text('База медикаментов'),
         ),
-      ),
-    );
+        body: new Container(
+            color: Colors.green[100],
+            child: ListView(
+              children: <Widget>[
+                new Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    new Column(
+                      children: <Widget>[
+                        new Table(
+                            defaultVerticalAlignment:
+                                TableCellVerticalAlignment.middle,
+                            children: [
+                              new TableRow(children: [
+                                new RaisedButton(
+                                  child: new Text('Сканировать товар.'),
+                                  onPressed: scan,
+                                ),
+                                new RaisedButton(
+                                  child: new Text('Список лекарств в базе.'),
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/drugs_list');
+                                  },
+                                ),
+                              ]),
+                              new TableRow(children: [
+                                new RaisedButton(
+                                  child: new Text('Очистить список.'),
+                                  onPressed: clearListDrugs,
+                                ),
+                                new RaisedButton(
+                                  child: new Text('Отправить данные.'),
+                                  onPressed: () {},
+                                ),
+                              ]),
+                            ]),
+                      ],
+                    ),
+                    new Column(
+                      children: <Widget>[
+                        new Table(children: [
+                          new TableRow(children: [
+                            Text(
+                              "Код товара",
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              "Количество",
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              "Дом/Дача",
+                              textAlign: TextAlign.center,
+                            )
+                          ]),
+                        ]),
+                      ],
+                    ),
+                    new Table(
+                      children: [
+                        for (var i in allCodes)
+                          TableRow(children: [
+                            Text(
+                              (allCodes.indexOf(i.toString()) + 1).toString() +
+                                  ") " +
+                                  i,
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(
+                              '',
+                              textAlign: TextAlign.center,
+                            ),
+                            Checkbox(
+                              //                  key: Key(i.toString()),
+                              value: monVal,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  monVal = value;
+                                });
+                              },
+                            )
+                          ])
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            )));
   }
 
   Future scan() async {
@@ -71,8 +129,13 @@ class _MainScreenState extends State<MainScreen> {
     setState(() {});
     return allCodes;
   }
-}
 
+  void clearListDrugs() {
+    setState(() {
+      allCodes = [];
+    });
+  }
+}
 
 class DrugListScreen extends StatelessWidget {
   @override
